1e7f98bde6fdb37e1a3930d04c13d57561e80cc0
terna <49841386+nameterzaza55@users.noreply.github.com>
2020-03-19 11:25:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@
 
 	$replyToken = $deCode['events'][0]['replyToken'];
 
-	$messages = "ถามหน่อย";
+	$messages = [];
 	$messages['replyToken'] = $replyToken;
 	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
 
